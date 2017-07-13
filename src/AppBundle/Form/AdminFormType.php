9c6f4d034387038c4f9d367913bb1a65edc74077
AlecMcAwesome <fredi-petersen93@hotmail.com>
2017-07-13 13:00:35 +0200

    changed AdminFormType

--- src/AppBundle/Form/AdminFormType.php
+++ src/AppBundle/Form/AdminFormType.php
@@ -2,7 +2,12 @@
 
 namespace AppBundle\Form;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,13 +16,16 @@ class AdminFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
             $builder
-                ->add('username')
-                ->add('email')
-                ->add('password')
-                ->add('confirmpassword')
-                ->add('bio')
-                ->add('role')
-                ;
+                ->add('username', TextType::class)
+                ->add('email', EmailType::class)
+                ->add('password', PasswordType::class)
+                ->add('confirmpassword', PasswordType::class)
+                ->add('bio', TextareaType::class)
+                ->add('role', ChoiceType::class, array(
+                    'choices' => array(
+                        'ROLE_USER' => false,
+                        'ROLE_ADMIN' => true,
+                    )));
     }
 
 
